81b711bfab77151bcdc76374ff18b6858c4846c8
ROHIT12-bit <reddyrohit823@gmail.com>
2026-01-09 13:05:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from Bot import Bot
+try:
+    from Bot import Bot
+except ModuleNotFoundError:
+    try:
+        from bot import Bot
+    except ModuleNotFoundError:
+        try:
+            from plugins.Bot import Bot
+        except ModuleNotFoundError:
+            from plugins.bot import Bot
 
 if __name__ == "__main__":
     Bot().run()
