70ec1fd3dae6a805e400698eb16885a0beb75e30
ROHIT12-bit <reddyrohit823@gmail.com>
2026-01-09 13:17:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -131,8 +131,11 @@ class Bot(Client):
             asyncio.create_task(keep_alive())
 
     async def stop(self, *args):
-        await super().stop()
+    try:
         await self.send_message(LOG_CHANNEL, "❌ Bot Stopped!")
+    except Exception:
+        pass
+    await super().stop()
 
 # 🔹 Log New Users
 @Bot.on_message(filters.command("start") & filters.private)
